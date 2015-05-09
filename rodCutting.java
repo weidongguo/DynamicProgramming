08d087de0db16df2ab04b8bf0b1ff115b43a724c
Weidong Guo <weidongguo@live.com>
2015-05-08 20:41:44 -0700

    updated case study rodCutting to include the cost of cutting

--- rodCutting.java
+++ rodCutting.java
@@ -1,27 +1,32 @@
 public class rodCutting{
 	
-	static int maxRevenue(int r[], int price[],int n){
-		int max = price[1] + r[n-1];	
-		for(int i = 2; i <= n; i++){ // O(n)
-			if( max < price[i] + r[n-i])//set the max revenue by comparing every single way of cutting the rod	
-				max = price[i] + r[n-i]; 	
-		}
-		return max;
-	}
 
-	static void revenue(int price[], int n){
+	static void revenue(int price[], int n, int cost){
 	/* r_n = max { p_i + r_n-i } for i from 1 to n*/
-		int r[] = new int[n+1];
-		r[0] = price[0]; //just 0; revenue for a length 0 rod is just $0
+		int r[] = new int[n+1]; int s[] = new int[n+1];
+		s[0] = 0;//cannot cut if there is no rod; 
+		s[1] = 1;//cut a rod with length 1 at the length 1, meaning no cut;
+		r[0] = 0; //just 0; revenue for a length 0 rod is just $0
 		r[1] = price[1]; //revenue of length 1 is always the price for length 1	
-		System.out.print(r[1] + " ");
+		System.out.print("r[i]: "); 
 			
-		for(int i = 2; i <= n; i++){ // O(n)
-			r[i] = maxRevenue(r, price, i);  //O(n)
-			System.out.print(r[i] + " ");	// max revenue for rod of length i
+		for(int j = 1; j <= n; j++){ // O(n)
+			int max = price[j];//price for not cutting at all	
+			s[j] = j;	 //cut rod of length j at length j -> no cut
+			for(	int k = 1; k<= j-1; k++) //O(n);
+				if( max < price[k] + r[j-k] - cost){
+					max = price[k] + r[j-k] - cost;
+					s[j] = k;	
+				}
+			r[j] = max;			
+			System.out.print(r[j] + " ");	//max revenue for rod of length i
 		}//O(n^2);
 		
-		
+		/* print out the best cuts for each size of rod	*/
+		System.out.print("\ns[i]: ");
+		for(int i = 1 ; i < s.length; i++){
+			System.out.print(s[i] + " " );
+		}
 		System.out.println();
 	}
 }
--- rodCuttingTest.java
+++ rodCuttingTest.java
@@ -8,8 +8,12 @@ public class rodCuttingTest{
 	/* length i = array index i
 	 * price p_i = price[i] */
 		int price[] = {0, 1, 5, 8, 9, 10}; //length 0 is not used
-		rodCutting.revenue(price, 5);
+		rodCutting.revenue(price, 5, 0);
 		int price2[] = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
-		rodCutting.revenue(price2, price2.length-1);
+		rodCutting.revenue(price2, price2.length-1, 1);
+		
+		int price3[] = {0, 1, 4, 5, 9, 10, 12, 15, 18, 19, 20};
+		rodCutting.revenue(price3, price3.length-1, 1);
+
 	}
 }
